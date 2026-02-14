fb9cba8918906a9400912e77c37a37a771f76f9d
guang-huh <81838234+guang-huh@users.noreply.github.com>
2026-02-13 21:51:50 -0500

    Format code in main.py

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 """Module providing greeting functions."""
-def hello():
-    print("hi")
-
-
-def bye():
-    print("bye")
-
-
-if __name__ == "__main__":
-    hello()
+ def hello():
+     print("hi")
+ 
+ 
+ def bye():
+     print("bye")
+ 
+ 
+ if __name__ == "__main__":
+     hello()
