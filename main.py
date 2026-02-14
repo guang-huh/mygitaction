60b534060a03943ba33fcc47ce1975f99c23b808
guang-huh <81838234+guang-huh@users.noreply.github.com>
2026-02-13 21:40:46 -0500

    Fix indentation and remove extra blank lines

--- main.py
+++ main.py
@@ -1,14 +1,11 @@
 """Module providing greeting functions."""
-
 def hello():
     """Prints a greeting."""
     print("hi")
 
-
 def bye():
     """Prints a farewell."""
     print("bye")
-
-
+    
 if __name__ == "__main__":
     hello()
